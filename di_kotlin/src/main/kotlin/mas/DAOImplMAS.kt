0704a7c8657fa47598be0e93fdeaee9f8d9d7b71
Dac Linh Giao Nguyen <dlg.nguyen@gmail.com>
2024-04-24 19:03:57 +0200

    Implement DAOImplMAS

---
+++ di_kotlin/src/main/kotlin/mas/DAOImplMAS.kt
@@ -0,0 +1,29 @@
+/*
+ * Dependency injection project.
+ * Dac Linh Giao NGUYEN
+ * License MIT
+ *
+ * Dependency injection project in Kotlin.
+ * DAOImplMAS.kt
+ */
+package com.kmmania.mas
+
+import com.kmmania.api.DAO
+
+/**
+ * Implementation of DAO interface for MAS (Maximal Aerobic Speed) data.
+ *
+ * @author Dac Linh Giao NGUYEN
+ * @since 0.1
+ * @see DAO
+ */
+class DAOImplMAS : DAO {
+    /**
+     * Retrieves the Maximal Aerobic Speed (MAS) from the MAS data source.
+     * @return the MAS as a Double.
+     */
+    override fun getData(): Double {
+        println("Data source provides a MAS 17.5km/h")
+        return 17.5
+    }
+}
