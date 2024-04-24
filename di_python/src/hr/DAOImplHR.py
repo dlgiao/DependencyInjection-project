105a0affd570bb723dd84eb64a6e42c0656352b4
Dac Linh Giao Nguyen <dlg.nguyen@gmail.com>
2024-04-24 22:39:51 +0200

    Implement DAOImplHR

---
+++ di_python/src/hr/DAOImplHR.py
@@ -0,0 +1,34 @@
+"""
+Dependency Injection project in Python
+
+This Python project demonstrates dependency injection using constructor injection.
+It includes an API with two interface classes: DAO and Service.
+The DAO interface simulates data access, while the Service interface simulates data computation.
+Two versions (MAS and HR) implement constructor injection from DAO into Service layer.
+
+DAOImplHR Class Documentation
+
+This Python class implements the DAO interface to provide data retrieval functionality for version
+hr of the project.
+
+Author: Dac Linh Giao NGUYEN
+Date: 2024
+
+Usage:
+    - Instantiate DAOImplHR to access data from the HR data source.
+    - Call the get_data method to retrieve data.
+"""
+
+from src.api.DAO import DAO
+
+
+class DAOImplHR(DAO):
+    """
+    Concrete implementation of the DAO interface for version HR of the project.
+    This implementation retrieves data from the second data source.
+    """
+
+    def get_data(self):
+        """Retrieve data from the second data source."""
+        print("Data source provides a max heart rate 185bpm")
+        return 185
