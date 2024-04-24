d6e3db3c62868f2ba577fc9cce9f8405e8f2c9d9
Dac Linh Giao Nguyen <dlg.nguyen@gmail.com>
2024-04-24 23:19:59 +0200

    Implement DAO interface

---
+++ di_js/src/api/DAO.js
@@ -0,0 +1,18 @@
+/**
+ * Represents a Data Access Object (DAO) for accessing data.
+ *
+ * @author Dac Linh Giao NGUYEN
+ * @version 0.1
+ * @since 0.1
+ */
+class DAO {
+    /**
+     * Retrieves data from a data source.
+     *
+     * @returns {number} Implementation logic for retrieving data
+     */
+    getData() {
+    }
+}
+
+module.exports = DAO;
