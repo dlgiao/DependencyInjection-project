fe02d966f8108c441fd39e2a6779967bb89e684e
Dac Linh Giao Nguyen <dlg.nguyen@gmail.com>
2024-04-24 16:22:29 +0200

    Implement DAO interface

---
+++ di_java/src/main/java/com/kmmania/api/DAO.java
@@ -0,0 +1,17 @@
+package com.kmmania.api;
+
+/**
+ * Interface for Data Access Object (DAO).
+ * Implement this interface to provide methods for accessing data.
+ *
+ * @author Dac Linh Giao NGUYEN
+ * @version 0.1
+ */
+public interface DAO {
+    /**
+     * Method to retrieve data.
+     *
+     * @return the retrieved data as a double.
+     */
+    double getData();
+}
