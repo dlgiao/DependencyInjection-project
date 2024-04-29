07ea26006fb5d68d3dfcc6ef7cd36800a2b94234
Dac Linh Giao Nguyen <dlg.nguyen@gmail.com>
2024-04-29 11:15:36 +0200

    Update comments

--- di_go/api/api.go
+++ di_go/api/api.go
@@ -1,21 +1,15 @@
 // Package api defines the interfaces required for dependency injection.
 package api
 
-/**
- * Interface for Data Access Object (DAO).
- * Implement this interface to provide methods for accessing data.
- */
-
+// Dao is the interface for Data Access Object.
+// Implement this interface to provide methods for accessing data.
 type Dao interface {
 	// GetData retrieves the data and returns a float64.
 	GetData() float64
 }
 
-/**
- * Interface for a service that performs computation based on data provided by a DAO (Data Access Object).
- * Implement this interface to provide methods for computing data.
- */
-
+// Service is the Interface that performs computation based on data provided by a Dao (Data Access Object).
+// Implement this interface to provide methods for computing data.
 type Service interface {
 	// Compute performs computations based on the data retrieved by the Dao and returns a float64.
 	Compute() float64
