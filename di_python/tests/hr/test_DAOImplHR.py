23a0e5eeb55491e6a0edda692dbcd32092c5099c
Dac Linh Giao Nguyen <dlg.nguyen@gmail.com>
2024-05-03 15:01:42 +0200

    Update

--- di_python/tests/hr/test_DAOImplHR.py
+++ di_python/tests/hr/test_DAOImplHR.py
@@ -1,29 +1,14 @@
-from unittest.mock import MagicMock, patch
-
-import pytest
-
 from src.hr.DAOImplHR import DAOImplHR
 
 
-@pytest.fixture
-def dao_impl_hr():
-    return MagicMock(spec=DAOImplHR)
+class TestDAOImplHR:
+    def setup_method(self, method):
+        print(f"Setting up {method}")
+        self.DAOImplHR = DAOImplHR()
 
+    def teardown_method(self, method):
+        print(f"Tearing down {method}")
+        del self.DAOImplHR
 
-def test_dao_impl_hr_get_data(dao_impl_hr):
-    """get_data() method should retrieve data from DAO."""
-    # Create a mock of get_data() method
-    with patch.object(dao_impl_hr, "get_data") as mock_get_data:
-        # Configure the mock behavior
-        mock_get_data.return_value = 185
-
-        # Call the method under test
-        result = dao_impl_hr.get_data()
-
-        # Verify the expected behavior
-        assert result == 185
-        mock_get_data.assert_called_once()
-
-
-if __name__ == '__main__':
-    pytest.main([__file__])
+    def test_get_data(self):
+        assert self.DAOImplHR.get_data() == 185
