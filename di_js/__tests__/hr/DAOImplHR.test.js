d57a56856d6e67d6e81687b42f2553777f155706
Dac Linh Giao Nguyen <dlg.nguyen@gmail.com>
2024-05-03 23:52:32 +0200

    Update

--- di_js/__tests__/hr/DAOImplHR.test.js
+++ di_js/__tests__/hr/DAOImplHR.test.js
@@ -1,16 +1,20 @@
 const DAOImplHR = require('../../src/hr/DAOImplHR');
 
 describe('DAOImplHR', () => {
+    let daoImplHR;
+
+    beforeEach(() => {
+        daoImplHR = new DAOImplHR();
+    });
+
     describe('getData()', () => {
         it('should retrieve data from data source HR and return 185', () => {
-            const daoImplHR = new DAOImplHR();
             const result = daoImplHR.getData();
             expect(result).toBe(185);
         });
 
         it('should log a message indicating data retrieval from source HR', () => {
-            const daoImplHR = new DAOImplHR();
-            const consoleSpy = jest.spyOn(console, 'log');
+            const consoleSpy = jest.spyOn(console, "log");
             daoImplHR.getData();
             expect(consoleSpy).toHaveBeenCalledWith('Data source provides a max heart rate 185bpm');
             consoleSpy.mockRestore();
