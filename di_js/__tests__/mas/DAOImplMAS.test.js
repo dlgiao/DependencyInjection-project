8fbbf04d51b11391aec13bcab45058ea3f5ab0d7
Dac Linh Giao Nguyen <dlg.nguyen@gmail.com>
2024-05-03 23:56:05 +0200

    Update

--- di_js/__tests__/mas/DAOImplMAS.test.js
+++ di_js/__tests__/mas/DAOImplMAS.test.js
@@ -1,15 +1,19 @@
 const DAOImplMAS = require('../../src/mas/DAOImplMAS');
 
 describe('DAOImplMAS', () => {
+    let daoImplMAS;
+
+    beforeEach(() => {
+        daoImplMAS = new DAOImplMAS();
+    })
+
     describe('getData()', () => {
         it('should retrieve data from data source 1 and return 17.5', () => {
-            const daoImplMAS = new DAOImplMAS();
             const result = daoImplMAS.getData();
             expect(result).toBe(17.5);
         });
 
         it('should log a message indicating data retrieval from source 1', () => {
-            const daoImplMAS = new DAOImplMAS();
             const consoleSpy = jest.spyOn(console, 'log');
             daoImplMAS.getData();
             expect(consoleSpy).toHaveBeenCalledWith('Data source provides a MAS 17.5km/h');
