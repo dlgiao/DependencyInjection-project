c52f6f8d6fbc75f77dc736e0adba044ab82163dc
Dac Linh Giao Nguyen <dlg.nguyen@gmail.com>
2024-05-07 00:02:54 +0200

    Update unit tests with github.com/stretchr/testify

--- di_go/hr/hr_test.go
+++ di_go/hr/hr_test.go
@@ -1,44 +1,42 @@
 package hr
 
 import (
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/mock"
 	"testing"
 )
 
 // MockDaoImplHr is a mock implementation of Dao for testing purposes.
-type MockDaoImplHr struct{}
+type MockDaoImplHr struct {
+	mock.Mock
+}
 
 // GetData retrieves dummy data for testing.
-func (dao MockDaoImplHr) GetData() float64 {
+func (dao *MockDaoImplHr) GetData() float64 {
 	return 200 // You can change this value to any desired test data
 }
 
 func TestHRGetData(t *testing.T) {
-	// Create a mock Dao instance
+	// Given
 	mockDao := MockDaoImplHr{}
 
-	// Call GetData method on the mock Dao
+	// When
 	result := mockDao.GetData()
 
-	// Check if the result matches the expected value
-	expected := 200.0 // You need to define the expected result based on the test data
-	if result != expected {
-		t.Errorf("GetData() returned %f, expected %f", result, expected)
-	}
+	// Then
+	expected := 200.0
+	assert.Equal(t, expected, result, "GetData() returned %f, expected %f", result, expected)
 }
 
 func TestHRCompute(t *testing.T) {
-	// Create a mock Dao instance
+	// Given
 	mockDao := MockDaoImplHr{}
+	service := ServiceImplHr{Dao: &mockDao}
 
-	// Create a Service instance with the mock Dao
-	service := ServiceImplHr{Dao: mockDao}
-
-	// Call Compute method on the Service
+	// When
 	result := service.Compute()
 
-	// Check if the result matches the expected value
+	// Then
 	expected := 150.0 // 75% of 200
-	if result != expected {
-		t.Errorf("Compute() returned %f, expected %f", result, expected)
-	}
+	assert.Equal(t, expected, result, "Compute() returned %f, expected %f", result, expected)
 }
