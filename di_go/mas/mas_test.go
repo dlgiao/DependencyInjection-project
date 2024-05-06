d5c748679cad4546b17498730bf69cda06e341d2
Dac Linh Giao Nguyen <dlg.nguyen@gmail.com>
2024-05-06 23:59:02 +0200

    Update unit tests with github.com/stretchr/testify

--- di_go/mas/mas_test.go
+++ di_go/mas/mas_test.go
@@ -1,44 +1,42 @@
 package mas
 
 import (
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/mock"
 	"testing"
 )
 
 // MockDaoImplMAS is a mock implementation of Dao for testing purposes.
-type MockDaoImplMAS struct{}
+type MockDaoImplMAS struct {
+	mock.Mock
+}
 
 // GetData retrieves dummy data for testing.
-func (dao MockDaoImplMAS) GetData() float64 {
-	return 20.0 // You can change this value to any desired test data
+func (dao *MockDaoImplMAS) GetData() float64 {
+	return 20.0
 }
 
 func TestMASGetData(t *testing.T) {
-	// Create a mock Dao instance
+	// Given
 	mockDao := MockDaoImplMAS{}
 
-	// Call GetData method on the mock Dao
+	// When
 	result := mockDao.GetData()
 
-	// Check if the result matches the expected value
-	expected := 20.0 // You need to define the expected result based on the test data
-	if result != expected {
-		t.Errorf("GetData() returned %f, expected %f", result, expected)
-	}
+	// Then
+	expected := 20.0
+	assert.Equal(t, expected, result, "GetData() returned %f, expected %f", result, expected)
 }
 
 func TestMASCompute(t *testing.T) {
-	// Create a mock Dao instance
+	// Given
 	mockDao := MockDaoImplMAS{}
+	service := ServiceImplMas{Dao: &mockDao}
 
-	// Create a Service instance with the mock Dao
-	service := ServiceImplMas{Dao: mockDao}
-
-	// Call Compute method on the Service
+	// When
 	result := service.Compute()
 
-	// Check if the result matches the expected value
+	// Then
 	expected := 14.0 // 70% of 20
-	if result != expected {
-		t.Errorf("Compute() returned %f, expected %f", result, expected)
-	}
+	assert.Equal(t, expected, result, "Compute() returned %f, expected %f", result, expected)
 }
